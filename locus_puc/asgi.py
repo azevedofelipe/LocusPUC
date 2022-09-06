17445ef4f679bedc728c940876b70ee5f2bad037
azevedofelipe <fazevedo2017@my.fit.edu>
2022-09-06 16:21:19 -0300

    Criar projeto django locus_puc

---
+++ locus_puc/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for locus_puc project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locus_puc.settings')
+
+application = get_asgi_application()
