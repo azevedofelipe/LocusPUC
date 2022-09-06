add090e768ddef7bf8144ee69ad7e68673b89eb3
azevedofelipe <fazevedo2017@my.fit.edu>
2022-09-06 16:43:05 -0300

    Criar app e tabela para lugares e mostrar no site admin

---
+++ lugar/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Tabela de lugares
+class Lugar(models.Model):
+    titulo = models.CharField(max_length=100)
+    slug = models.SlugField()
+    descricao = models.TextField()
+    thumb = models.ImageField(default='default.png',blank=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.titulo
