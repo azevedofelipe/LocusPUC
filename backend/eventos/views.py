c6bf5ea8ec7a0fbfe3bdcc097959116b867d4b5c
azevedofelipe <fazevedo2017@my.fit.edu>
2022-10-12 12:00:02 -0300

    Criar filtro de eventos

--- backend/eventos/views.py
+++ backend/eventos/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render
 from .models import Evento
 from rest_framework import generics
 from .serializers import EventoSerializer
+from django_filters import rest_framework as filters
 
 
 # API para listar todos os lugares e criar lugares [C]RUD
 class EventoList(generics.ListCreateAPIView):
+
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = {
+        'local__titulo' : ['contains'],
+        'titulo' : ['contains']
+    }
+
     serializer_class = EventoSerializer
-    queryset = Evento.objects.all()
+    queryset = Evento.objects.all().distinct()
 
 # API para update, delete e detalhes de lugares C[RUD]
 class EventoDetalhes(generics.RetrieveUpdateDestroyAPIView):
+        
     serializer_class = EventoSerializer
     queryset = Evento.objects.all()
