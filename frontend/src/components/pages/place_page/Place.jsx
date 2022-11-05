ada8f0c8cd38237dd631e37faa967ea42bf5d953
Rodrigosnrocha <rodrigosnr@al.insper.edu.br>
2022-11-05 18:35:27 -0300

    Ajeitar checkbox busca

---
+++ frontend/src/components/pages/place_page/Place.jsx
@@ -0,0 +1,20 @@
+import './Place.css'
+import Main from '../../templates/Main'
+import imgInicio from '../../../assets/imgs/inicio.jpg'
+import Header from '../../templates/Header'
+import { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Main>
+        <div className='home row d-flex align-items-center'>
+          <Header />
+          <div className='d-none d-sm-block img col-12 col-xl-6 offset-xl-1 gl-5'>
+            <img className='img-thumbnail' src={imgInicio} alt='Fachada do edifício Cardeal Leme' />
+          </div>
+        </div>
+      </Main>
+    )
+  }
+}
