558784d5615195b47c85bfb2d1cfc5ca760a9309
azevedofelipe <fazevedo2017@my.fit.edu>
2022-11-05 17:19:53 -0300

    Recuperação de senha por email

--- backend/conta/urls.py
+++ backend/conta/urls.py
@@ -1,5 +1,5 @@
 from backend.conta.serializers import AlterarSenhaSerializer
-from .views import AlterarSenhaView, AtualizarPerfilView, CadastroAPI, LoginAPI, UserAPI
+from .views import AlterarSenhaView, AtualizarPerfilView, CadastroAPI, LoginAPI, UserAPI, UserEmailAPI
 from knox import views as knox_views
 from django.urls import path
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('user/',UserAPI.as_view(),name='user'),
     path('alterar_senha/<int:pk>/', AlterarSenhaView.as_view(),name='alterar_senha'),                   # Alterar senha, <int:pk> id do user
     path('atualizar_perfil/<int:pk>/', AtualizarPerfilView.as_view(),name='atualizar_perfil'),          # Atualizar perfil, <int:pk> id do user
+    path('email/<str:email>',UserEmailAPI.as_view(),name='email'),                                      # GET usuario por email
 ]
