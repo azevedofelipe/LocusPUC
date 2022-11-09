46a2e2c66c2674c365158fdf53dbda18a171f8d3
azevedofelipe <fazevedo2017@my.fit.edu>
2022-11-08 21:00:34 -0300

    Atualizar API de alterar senha

--- backend/conta/serializers.py
+++ backend/conta/serializers.py
@@ -39,23 +39,16 @@ class CadastroSerializer(serializers.ModelSerializer):
 class AlterarSenhaSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True,validators=[validate_password])
     password2 = serializers.CharField(write_only=True,required=True)
-    old_password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ('old_password','password','password2')
+        fields = ('password','password2')
 
     def validate(self,attrs):
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({"password": "Senhas não coincidem"})
 
         return attrs
-
-    def validate_old_password(self,value):
-        user = self.context['request'].user
-        if not user.check_password(value):
-            raise serializers.ValidationError({"old_password": "Senha antiga incorreta"})
-        return value
 
     def update(self, instance, validated_data):
         user = self.context['request'].user
