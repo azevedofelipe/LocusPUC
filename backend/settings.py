9fc4df78b71f5db9430c746874eb47a3e850985c
azevedofelipe <78154519+azevedofelipe@users.noreply.github.com>
2023-05-22 15:38:18 -0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hr_l*%vvytltnj%ii)y6z1*k$sl%lgyp+u9rev&7woc^_2=*-d'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
